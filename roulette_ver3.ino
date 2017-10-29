092c9d00d2a152b0c8224cf7733fc9d30723cfed
TateIsuKanna <TateIsuKanna@users.noreply.github.com>
2017-10-29 13:20:34 +0900

    ボタン

--- roulette_ver3.ino
+++ roulette_ver3.ino
@@ -2,7 +2,10 @@ const int ACCELEROMETER_PORT = 8;
 const int SPEAKER_PORT = 9;
 const int PUMP_PORT = 10;
 const int RESTART_PORT = 11;
+const int RASPI_SOUND_PORT=0;
 const int LED = 49; // 9pin use
+const int BUTTON_PORT=12;
+const int DIFFICULTY_LED_FIRST_PORT=13;
 
 int current_LED_position;
 
@@ -12,10 +15,8 @@ unsigned long time_nagasa = 0;
 unsigned long EASY = 5000;
 unsigned long NOMAL = 10000;
 
-//20と22は何が違うの?
-
 void setup(){
-	for(int i=20;i<=LED;i++){
+	for(int i=22;i<=LED;i++){
 		pinMode(i,OUTPUT);
 	}
 
@@ -39,9 +40,13 @@ void judge(int difficulty){
 	for(long t=0;t<6000/pow(2,difficulty);t++){
 		if((digitalRead(ACCELEROMETER_PORT)==HIGH&&digitalRead(48)==HIGH)||(digitalRead(ACCELEROMETER_PORT)==HIGH&&digitalRead(47)==HIGH)){
 			while(true){
-				for(int j=20;j<=LED;j++)digitalWrite(j,HIGH);
+				for(int j=22;j<=LED;j++){
+					digitalWrite(j,HIGH);
+				}
 				delay(100);
-				for(int j=20;j<=LED;j++)digitalWrite(j,LOW);
+				for(int j=22;j<=LED;j++){
+					digitalWrite(j,LOW);
+				}
 				delay(100);
 			}
 		}else if(digitalRead(ACCELEROMETER_PORT)==HIGH){
@@ -55,11 +60,36 @@ void judge(int difficulty){
 	}        
 }
 
+int DIFFICULTY_LED_DATA[][4]=
+{
+	{1,1,1,1},
+	{1,0,0,0},
+	{0,1,0,0},
+	{0,0,1,0},
+	{0,0,0,1}
+};
+
+void set_difficulty(){
+	while(digitalRead(13)==HIGH);
+
+	unsigned long int count=0;
+	unsigned long int start_time_ms=millis();
+	while(millis()-start_time_ms<15000){
+		for(int i=0;i<4;++i){
+			digitalWrite(DIFFICULTY_LED_FIRST_PORT+i, DIFFICULTY_LED_DATA[count/200][i]);
+		}
+		if (digitalRead(13) == LOW) {
+			count++;
+			delay(10);
+		}
+	}
+}
 
 void loop(){  
-	//pump music
-	digitalWrite(0,HIGH);
-	digitalWrite(0,LOW);
+	set_difficulty();
+
+	digitalWrite(RASPI_SOUND_PORT,HIGH);
+	digitalWrite(RASPI_SOUND_PORT,LOW);
 	//start music
 	digitalWrite(1,HIGH);
 	delay(100);
