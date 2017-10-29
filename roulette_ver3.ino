5213fc26f58eaec756b49920c56ab226911722bf
TateIsuKanna <TateIsuKanna@users.noreply.github.com>
2017-10-29 10:42:48 +0900

    リファクタリング

--- roulette_ver3.ino
+++ roulette_ver3.ino
@@ -1,22 +1,21 @@
-#define BOTTON 8
-#define SPEAKER 9
-#define PUMP 10
-#define RESTART 11
-
-
+const int ACCELEROMETER_PORT = 8;
+const int SPEAKER_PORT = 9;
+const int PUMP_PORT = 10;
+const int RESTART_PORT = 11;
 const int LED = 49; // 9pin use
-int i = 0;
-int j = 0;
-long t = 0; 
-int judge;
+
+int current_LED_position;
+
 unsigned long time_m = 0;
 unsigned long time_n = 0;
 unsigned long time_nagasa = 0;
 unsigned long EASY = 5000;
 unsigned long NOMAL = 10000;
 
+//20と22は何が違うの?
+
 void setup(){
-	for(i=20;i<=LED;i++){
+	for(int i=20;i<=LED;i++){
 		pinMode(i,OUTPUT);
 	}
 
@@ -24,28 +23,42 @@ void setup(){
 	pinMode(0,INPUT);
 	pinMode(1,INPUT);
 
-	for(i=2;i<5;i++){
+	for(int i=2;i<5;i++){
 		pinMode(i,INPUT);
 	}
 
-	//加速度センサ
-	pinMode(BOTTON, INPUT);
-	//speaker
-	pinMode(SPEAKER, OUTPUT);
-	//pump
-	pinMode(PUMP,INPUT);
-	//restart
-	pinMode(RESTART,INPUT);
+	pinMode(ACCELEROMETER_PORT, INPUT);
+	pinMode(SPEAKER_PORT, OUTPUT);
+	pinMode(PUMP_PORT,INPUT);
+	pinMode(RESTART_PORT,INPUT);
 }
 
 //難易度設定
 
+void judge(int difficulty){
+	for(long t=0;t<6000/pow(2,difficulty);t++){
+		if((digitalRead(ACCELEROMETER_PORT)==HIGH&&digitalRead(48)==HIGH)||(digitalRead(ACCELEROMETER_PORT)==HIGH&&digitalRead(47)==HIGH)){
+			while(true){
+				for(int j=20;j<=LED;j++)digitalWrite(j,HIGH);
+				delay(100);
+				for(int j=20;j<=LED;j++)digitalWrite(j,LOW);
+				delay(100);
+			}
+		}else if(digitalRead(ACCELEROMETER_PORT)==HIGH){
+			while(true){
+				digitalWrite(current_LED_position,HIGH);
+				delay(100);
+				digitalWrite(current_LED_position,LOW);
+				delay(100);
+			}                        
+		}
+	}        
+}
+
 
 void loop(){  
-
-	//puｍｐ music
+	//pump music
 	digitalWrite(0,HIGH);
-
 	digitalWrite(0,LOW);
 	//start music
 	digitalWrite(1,HIGH);
@@ -53,173 +66,30 @@ void loop(){
 	digitalWrite(1,LOW);
 	delay(100);
 
-	for(i=25;i>22;i--){
-		tone( SPEAKER, 622);
+	for(int i=25;i>22;i--){
+		tone( SPEAKER_PORT, 622);
 		digitalWrite(i,HIGH);
 		delay(1000);
-		noTone(SPEAKER);
+		noTone(SPEAKER_PORT);
 		digitalWrite(i,LOW);
 		delay(1000);
 	}
 
-	judge=1;
-
-	switch(judge){
-		//easys
-		case 1:  
-			for(i=22;i<=LED;i++){
-				digitalWrite(i,HIGH);
-				tone( SPEAKER, 622);
-
-				for(t=0;t<5000;t++){
-					if((digitalRead(BOTTON)==HIGH&&digitalRead(48)==HIGH)||(digitalRead(BOTTON)==HIGH&&digitalRead(47)==HIGH)){
-						while(1){
-							for(j=20;j<=LED;j++)digitalWrite(j,HIGH);
-							delay(100);
-							for(j=20;j<=LED;j++)digitalWrite(j,LOW);
-							delay(100);
-						}
-					}else if(digitalRead(BOTTON)==HIGH){
-						while(1){
-							digitalWrite(i,HIGH);
-							delay(100);
-							digitalWrite(i,LOW);
-							delay(100);
-						}                        
-					}
-				}        
-
-
-				digitalWrite(i,LOW);
-				noTone(SPEAKER);
-
-				for(t=0;t<6000;t++){
-					if((digitalRead(BOTTON)==HIGH&&digitalRead(48)==HIGH)||(digitalRead(BOTTON)==HIGH&&digitalRead(47)==HIGH)){
-						while(1){
-							for(j=20;j<=LED;j++)digitalWrite(j,HIGH);
-							delay(100);
-							for(j=20;j<=LED;j++)digitalWrite(j,LOW);
-							delay(100);
-						}
-					}else if(digitalRead(BOTTON)==HIGH){
-						while(1){
-							digitalWrite(i,HIGH);
-							delay(100);
-							digitalWrite(i,LOW);
-							delay(100);
-						}                        
-					}
-				}            
-				break;
-			}
-
-			//nomal
-		case 2:  
-			for(i=22;i<=LED;i++){
-				digitalWrite(i,HIGH);
-				tone( SPEAKER, 622);
-
-				for(t=0;t<3000;t++){
-					if((digitalRead(BOTTON)==HIGH&&digitalRead(48)==HIGH)||(digitalRead(BOTTON)==HIGH&&digitalRead(47)==HIGH)){
-						while(1){
-							for(j=20;j<=LED;j++)digitalWrite(j,HIGH);
-							delay(100);
-							for(j=20;j<=LED;j++)digitalWrite(j,LOW);
-							delay(100);
-						}
-					}else if(digitalRead(BOTTON)==HIGH){
-						while(1){
-							digitalWrite(i,HIGH);
-							delay(100);
-							digitalWrite(i,LOW);
-							delay(100);
-						}                        
-					}            
-				}        
-
-
-				digitalWrite(i,LOW);
-				noTone(SPEAKER);
-
-				for(t=0;t<3000;t++){
-					if((digitalRead(BOTTON)==HIGH&&digitalRead(48)==HIGH)||(digitalRead(BOTTON)==HIGH&&digitalRead(47)==HIGH)){
-						while(1){
-							for(j=20;j<=LED;j++)digitalWrite(j,HIGH);
-							delay(100);
-							for(j=20;j<=LED;j++)digitalWrite(j,LOW);
-							delay(100);
-						}
-					}else if(digitalRead(BOTTON)==HIGH){
-						while(1){
-							digitalWrite(i,HIGH);
-							delay(100);
-							digitalWrite(i,LOW);
-							delay(100);
-						}                        
-					}
-				}            
-
-				break;
-			}
-
-			//hard
-		case 3:  
-			for(i=22;i<=LED;i++){
-				digitalWrite(i,HIGH);
-				tone( SPEAKER, 622);
-
-				for(t=0;t<1500;t++){
-					if((digitalRead(BOTTON)==HIGH&&digitalRead(48)==HIGH)||(digitalRead(BOTTON)==HIGH&&digitalRead(47)==HIGH)){
-						while(1){
-							for(j=20;j<=LED;j++)digitalWrite(j,HIGH);
-							delay(100);
-							for(j=20;j<=LED;j++)digitalWrite(j,LOW);
-							delay(100);
-						}
-					}else if(digitalRead(BOTTON)==HIGH){
-						while(1){
-							digitalWrite(i,HIGH);
-							delay(100);
-							digitalWrite(i,LOW);
-							delay(100);
-						}                        
-					}            
-				}        
-
-
-				digitalWrite(i,LOW);
-				noTone(SPEAKER);
-
-				for(t=0;t<1500;t++){
-					if((digitalRead(BOTTON)==HIGH&&digitalRead(48)==HIGH)||(digitalRead(BOTTON)==HIGH&&digitalRead(47)==HIGH)){
-						while(1){
-							for(j=20;j<=LED;j++)digitalWrite(j,HIGH);
-							delay(100);
-							for(j=20;j<=LED;j++)digitalWrite(j,LOW);
-							delay(100);
-						}
-					}else if(digitalRead(BOTTON)==HIGH){
-						while(1){
-							digitalWrite(i,HIGH);
-							delay(100);
-							digitalWrite(i,LOW);
-							delay(100);
-						}                        
-					}
-				}            
-				break;          
-			}
-		default:
-			break;
+	int difficulty=0;//DEBUG
+	for(current_LED_position=22;current_LED_position<=LED;current_LED_position++){
+		digitalWrite(current_LED_position,HIGH);
+		tone( SPEAKER_PORT, 622);
+		judge(difficulty);
+		digitalWrite(current_LED_position,LOW);
+		noTone(SPEAKER_PORT);
+		judge(difficulty);
 	}
 
-	if(i==49){
-		while(digitalRead(RESTART)==LOW){
-			digitalWrite(LED,HIGH);
-			delay(100);
-			digitalWrite(LED,LOW);
-			delay(100);
-		}
+	while(digitalRead(RESTART_PORT)==LOW){
+		digitalWrite(LED,HIGH);
+		delay(100);
+		digitalWrite(LED,LOW);
+		delay(100);
 	}
 }
 
